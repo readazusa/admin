483dbdd631806bef0c48d17e9b6f3036a1275c37
smc <smc@oceansoft.com.cn>
2016-03-29 22:21:09 +0800

    增加防止重复提交token机制

--- src/main/java/net/sunmingchun/www/interceptor/TokenInterceptor.java
+++ src/main/java/net/sunmingchun/www/interceptor/TokenInterceptor.java
@@ -1,9 +1,11 @@
 package net.sunmingchun.www.interceptor;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * net.sunmingchun.www.interceptor
@@ -15,42 +17,18 @@ public class TokenInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        if (handler instanceof HandlerMethod) {
-//            HandlerMethod handlerMethod = (HandlerMethod) handler;
-//            Method method = handlerMethod.getMethod();
-////            Token annotation = method.getAnnotation(Token.class);
-////            if (annotation != null) {
-////                boolean needSaveSession = annotation.save();
-////                if (needSaveSession) {
-////                    request.getSession(false).setAttribute("token", UUID.randomUUID().toString());
-////                }
-////                boolean needRemoveSession = annotation.remove();
-////                if (needRemoveSession) {
-////                    if (isRepeatSubmit(request)) {
-////                        return false;
-////                    }
-////                    request.getSession(false).removeAttribute("token");
-////                }
-//            }
-//            return true;
-//        } else {
-////            return super.preHandle(request, response, handler);
-//        }
-         return true;
+        boolean validateToken = false;
+        HttpSession session = request.getSession();
+        String serverToken = (String) session.getAttribute("token");
+        String clientToken = request.getParameter("token");
+        if(StringUtils.isBlank(serverToken)){
+            validateToken = true;
+            session.setAttribute("token",clientToken);
+        }else if(!serverToken.equals(clientToken)){
+            validateToken = true;
+            session.setAttribute("token",clientToken);
+        }
+        return validateToken;
     }
 
-    private boolean isRepeatSubmit(HttpServletRequest request) {
-//        String serverToken = (String) request.getSession(false).getAttribute("token");
-//        if (serverToken == null) {
-//            return true;
-//        }
-//        String clinetToken = request.getParameter("token");
-//        if (clinetToken == null) {
-//            return true;
-//        }
-//        if (!serverToken.equals(clinetToken)) {
-//            return true;
-//        }
-        return false;
-    }
 }
