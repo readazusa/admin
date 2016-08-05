8cd3a55697ada16ec1deac3a004dfa3dcab713b8
smc <smc@oceansoft.com.cn>
2016-08-05 21:54:07 +0800

    ftp上传组件的编写

--- src/main/java/net/sunmingchun/www/util/FtpUtils.java
+++ src/main/java/net/sunmingchun/www/util/FtpUtils.java
@@ -1,16 +1,49 @@
 package net.sunmingchun.www.util;
 
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * net.sunmingchun.www.util
  * Created by smc
  * date on 2016/3/1.
  * Email:sunmch@163.com
+ * ftp文件上传
  */
 public final class FtpUtils {
+
+    private String username;
+
+    private String password;
+
+    private int port;
+
+    private String ip;
+
+    private FTPClient ftpClient = null;
+
 
     private static FtpUtils ftpUtils = null;
 
     private  FtpUtils(){
+         if(ftpClient == null){
+             ftpClient = new FTPClient();
+             try {
+                 ftpClient.connect(ip);
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+         }
+        if(!ftpClient.isConnected()){
+            try {
+                ftpClient.login(username,password);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
 
     }
 
@@ -20,6 +53,18 @@ public final class FtpUtils {
         }
         return ftpUtils;
     }
+
+    /**
+     *
+     * @param remote   远程ftp文件夹
+     * @param inputStream   //需要上传的文件流
+     * @return
+     */
+    public String sendFile(String remote, InputStream inputStream){
+        return null;
+    }
+
+
 
 
     
